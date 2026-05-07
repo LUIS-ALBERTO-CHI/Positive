117d5de22239723ff904aafc16a93ce13427d8e1
Luis A. Casanova <132479713+LuisCasanova11@users.noreply.github.com>
2026-05-06 23:14:50 -0600

    Change to CommonJS for Vercel serverless functions

--- api/send-notification.js
+++ api/send-notification.js
@@ -12,7 +12,7 @@ const pool = new Pool({
     }
 });
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         res.status(405).json({ error: 'Method not allowed' });
         return;
--- api/vapid-public-key.js
+++ api/vapid-public-key.js
@@ -1,6 +1,6 @@
 // api/vapid-public-key.js
 
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
     if (req.method !== 'GET') {
         res.status(405).json({ error: 'Method not allowed' });
         return;
